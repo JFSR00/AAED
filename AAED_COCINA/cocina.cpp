0b2b557f0bdbbe55fbcc1169141042761d63e5fc
JFSR00 <juanfsr00@gmail.com>
2020-06-17 19:03:14 +0200

    Comienzo problema TAD Cocina

---
+++ AAED_COCINA/cocina.cpp
@@ -0,0 +1,10 @@
+/*
+ * cocina.cpp
+ *
+ *  Created on: 17 jun. 2020
+ *      Author: JFSR00
+ */
+
+
+
+
---
+++ AAED_COCINA/cocina.hpp
@@ -0,0 +1,15 @@
+/*
+ * cocina.hpp
+ *
+ *  Created on: 17 jun. 2020
+ *      Author: JFSR00
+ */
+
+#ifndef COCINA_HPP_
+#define COCINA_HPP_
+
+
+
+
+
+#endif /* COCINA_HPP_ */
